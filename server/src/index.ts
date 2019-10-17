1b4846b4ea0c9167a4fbacbd880b276398c8ae79
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-10-16 22:00:08 -0400

    added apploserver and grapghql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,50 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
-createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+// import { createConnection } from "typeorm";
+//import { User } from "./entity/User";
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
 
-}).catch(error => console.log(error));
+(async () => {// async and await code. logic to start server
+    const app = express();
+    app.get(`/`, (_reg, response) => response.send('hello'))
+
+    const apolloServer = new ApolloServer({//grapghql define graph ql schema
+        typeDefs: `
+            type Query {
+                hello: String!
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => 'hello world'
+            }
+        }
+    })
+    //lastly add 
+    apolloServer.applyMiddleware({ app });//added grapgh ql to the app
+    app.listen(4000, () => {
+        console.log('server has started')
+    });
+})();
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
