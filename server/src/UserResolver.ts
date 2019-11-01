8acb552f47dadb17febaabfd735179066fe75fe6
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-11-01 19:30:18 -0400

    added accessToken

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -3,14 +3,11 @@ import { hash, compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { User } from './entity/User';
 
-
-
-//allows the query of acessToken 
-ObjectType()
+//can query accesstokens to test
+@ObjectType()
 class LoginResponse {
   @Field()
   accessToken: string
-
 }
 
 @Resolver()
@@ -45,30 +42,28 @@ export class UserResolver {// graphql schema goes in here
   }
 
   //login mutation/resolver
-  //login in response is returned
-  //once person logs in token is jenerated
   @Mutation(() => LoginResponse)// mutations is what we want changable in the database
   async login(
     @Arg('email') email: string,// name of gql schema and type arguments
-    @Arg('password') password: string
-  ): Promise<LoginResponse>{//promise of loginResponse which holds  token when user logs in
+    @Arg('password') password: string,
+  ): Promise<LoginResponse> {// once user logins access token is generated
     const user = await User.findOne({ where: { email } });// search to see if email matches email
     //if no user
     if (!user) {
       throw new Error('invalid login')
     }
 
-    //compare passwords// import compare() from b crypt
-    const valid = compare(password, user.password)
+    //compare passwords// import compare from b crypt
+    const valid = await compare(password, user.password)
 
     //if not valid
     if (!valid) {
       throw new Error('invalid user or password')
     }
     //sucessful login
-    //accessToken
+    //generates acess token 
     return {
-      accessToken: sign({userId: user.id,}, 'ghtesdtr90al', {expiresIn: '15m'})//signfunction is used to asing token//store anything u want
-    } // it word
+      accessToken: sign({userId: user.id}, 'retgasutkalw', {expiresIn: '15m'})
+    }
   }
 }
